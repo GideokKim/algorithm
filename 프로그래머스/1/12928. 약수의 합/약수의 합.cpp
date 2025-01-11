eb85c0bf4281dadf6d31122ee4a343095e99bdac
Gideok Kim <ikid4127@naver.com>
2025-01-12 01:08:35 +0900

    [level 1] Title: 약수의 합, Time: 0.02 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    for (int number = 1; number <= n; ++number) {
+        if (n % number == 0) {
+            answer += number;
+        }
+    }
+    return answer;
+}

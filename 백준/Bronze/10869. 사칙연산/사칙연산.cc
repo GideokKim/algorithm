d216fbd6eae7078b7e2bed2a7dd11ad533ebabea
Gideok Kim <ikid4127@naver.com>
2025-01-22 22:46:50 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#pragma warning(disable:4996)
+using namespace std;
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	cin >> a >> b;
+	cout << a + b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+	cout << a / b << endl;
+	cout << a % b << endl;
+	return 0;
+}

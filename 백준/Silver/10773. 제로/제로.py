f61fbc02cdc2fec4420275d46859d8733e1fd36e
Gideok Kim <ikid4127@naver.com>
2025-02-01 21:46:22 +0900

    [Silver IV] Title: 제로, Time: 108 ms, Memory: 114604 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,14 @@
+import sys
+
+K = int(sys.stdin.readline().strip())
+numbers = []
+
+for _ in range(K):
+    number = int(sys.stdin.readline().strip())
+    if number == 0:
+        if numbers:
+            numbers.pop()
+    else:
+        numbers.append(number)
+
+print(sum(numbers))

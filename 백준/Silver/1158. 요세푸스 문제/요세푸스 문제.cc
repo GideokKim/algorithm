3aaaa45a855d119bcbe53437772ba6fd93c6ce40
Gideok Kim <ikid4127@naver.com>
2025-02-03 17:44:49 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 48 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -0,0 +1,55 @@
+#define BOJ_SUBMIT
+#ifndef BOJ_1000_SRC_1158_JOSEPHUS_PROBLEM_H_
+#define BOJ_1000_SRC_1158_JOSEPHUS_PROBLEM_H_
+
+#include <iostream>
+#include <queue>
+
+class JosephusProblem {
+ public:
+  void SetInputs() {
+    int n;
+    std::cin >> n >> k;
+
+    for (int i = 0; i < n; ++i) {
+      numbers.push(i + 1);
+    }
+  }
+
+  void Calculate() {
+    while (!numbers.empty()) {
+      for (int i = 0; i < k - 1; ++i) {
+        numbers.push(numbers.front());
+        numbers.pop();
+      }
+      result.push_back(numbers.front());
+      numbers.pop();
+    }
+  }
+
+  void PrintResult() {
+    std::cout << '<';
+    for (size_t index = 0; index < result.size() - 1; ++index) {
+      std::cout << result[index];
+      std::cout << ", ";
+    }
+    std::cout << result.back() << '>';
+  }
+
+ private:
+  int k;
+  std::queue<int> numbers;
+  std::vector<int> result;
+};
+
+#ifdef BOJ_SUBMIT
+int main() {
+  JosephusProblem josephus_problem;
+  josephus_problem.SetInputs();
+  josephus_problem.Calculate();
+  josephus_problem.PrintResult();
+  return 0;
+}
+#endif  // BOJ_SUBMIT
+
+#endif  // BOJ_1000_SRC_1158_JOSEPHUS_PROBLEM_H_

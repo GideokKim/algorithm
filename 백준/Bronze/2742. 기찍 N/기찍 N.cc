d3b34dc864ed46cd1d7a1e7eb023af34b7bc5b7e
Gideok Kim <ikid4127@naver.com>
2025-04-07 08:03:45 +0900

    [Bronze IV] Title: 기찍 N, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,40 @@
+#define BOJ_SUBMIT
+#ifndef BOJ_2000_SRC_2741_PRINT_N_H_
+#define BOJ_2000_SRC_2741_PRINT_N_H_
+
+#include <iostream>
+
+class PrintN {
+ public:
+  void SetInputs() {
+    std::cin >> n;
+    std::cin.ignore();
+  }
+
+  void Calculate() {}
+
+  void PrintResult() {
+    for (int i = n; i >= 1; i--) {
+      std::cout << i << '\n';
+    }
+  }
+
+ private:
+  int n;
+};
+
+#ifdef BOJ_SUBMIT
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+
+  PrintN print_n;
+  print_n.SetInputs();
+  print_n.Calculate();
+  print_n.PrintResult();
+  return 0;
+}
+#endif  // BOJ_SUBMIT
+
+#endif  // BOJ_2000_SRC_2741_PRINT_N_H_

7e8fd78fadee68d5c179ce1b5cabd9940ab5c78d
Gideok Kim <ikid4127@naver.com>
2025-04-25 15:55:52 +0900

    [level 0] Title: A로 B 만들기, Time: 0.07 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int solution(string before, string after) {
+    int answer = 0;
+    std::sort(before.begin(), before.end());
+    std::sort(after.begin(), after.end());
+    if (before == after) {
+        answer = 1;
+    }
+    return answer;
+}

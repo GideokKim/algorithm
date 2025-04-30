a468ff2edbd70fc0696c05f494221f321086b1a7
Gideok Kim <ikid4127@naver.com>
2025-04-30 19:28:33 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 1;
+    int factorial = 1;
+    while (factorial <= n) {
+        answer++;
+        factorial *= answer;
+    }
+    answer--;
+    return answer;
+}

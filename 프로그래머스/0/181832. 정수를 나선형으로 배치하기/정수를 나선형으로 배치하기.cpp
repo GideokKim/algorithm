adfe76b0097050c156a27eb4c367f000236099b4
Gideok Kim <ikid4127@naver.com>
2025-07-21 08:22:18 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.28 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(int n) {
+    vector<vector<int>> answer;
+    answer.resize(n, std::vector<int>(n, 0));
+    if (n == 1) {
+        answer[0][0] = 1;
+        return answer;
+    }
+    
+    std::vector<int> dx = {0, 1, 0, -1};
+    std::vector<int> dy = {1, 0, -1, 0};
+    int index = 0;
+    int x = 0;
+    int y = 0;
+    for (int i = 1; i <= n * n; ++i) {
+        answer[x][y] = i;
+        x += dx[index];
+        y += dy[index];
+        if (n <= x || n <= y || x < 0 || y < 0) {
+            x -= dx[index];
+            y -= dy[index];
+            ++index;
+            index %= 4;
+            x += dx[index];
+            y += dy[index];
+        }
+        if (answer[x][y]) {
+            x -= dx[index];
+            y -= dy[index];
+            ++index;
+            index %= 4;
+            x += dx[index];
+            y += dy[index];
+        }
+    }
+    return answer;
+}

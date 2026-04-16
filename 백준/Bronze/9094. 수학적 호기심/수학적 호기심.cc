f376c4b96d5b4b1425a95a05cc0d0d77f54b8a8a
Gideok Kim <ikid4127@naver.com>
2026-04-16 21:51:14 +0900

    [Bronze III] Title: 수학적 호기심, Time: 496 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/9094. 수학적 호기심/수학적 호기심.cc
+++ 백준/Bronze/9094. 수학적 호기심/수학적 호기심.cc
@@ -13,10 +13,9 @@ int main()
 		int satisfy = 0;
 		for (int a = 1; a < n - 1; a++)
 		{
-			int aaplusm = a * a + m;
 			for (int b = a + 1; b < n; b++)
 			{
-				if ((aaplusm + b * b) % (a * b) == 0)
+				if ((a * a + b * b + m) % (a * b) == 0)
 					satisfy++;
 			}
 		}
